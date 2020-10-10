484767e4f207094feab34b6dccb8ab2182b7d65e
Paul Blaise <paulloreto@gmail.com>
2020-10-10 18:50:00 +0800

    first commit instead of forking

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=paul
+export POSTGRES_PASSWORD=Rdspaul1
+export POSTGRES_HOST=cdr.cdcv3tlk8exq.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-331077312353-dev
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
